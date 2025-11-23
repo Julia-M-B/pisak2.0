b0d4f6127eea0ad6344481b9e095fd69f85c7c2d
Julia Bochniarz-Paziewska <jm.bochniarz@gmail.com>
2025-11-23 23:59:38 +0100

    refactor component

--- pisak/components/display_keyboard_component.py
+++ pisak/components/display_keyboard_component.py
@@ -1,16 +1,23 @@
 import os
+from typing import Any
+
 from pisak.components.keyboard import Keyboard, KeyboardType, ButtonManager, ButtonClickHandler
+from pisak.widgets.containers import PisakColumnWidget
+from pisak.scanning.strategies import BackNLevelsStrategy
 from pisak.widgets.stacked_widgets import PisakStackedWidget, ItemSwitchedHandler
 from pisak.widgets.text_display import PisakDisplay, TextEditionHandler
 
 
-class KeyboardDisplayComponent:
+class KeyboardDisplayComponent(PisakColumnWidget):
     def __init__(self, parent, scanning_manager, keyboards_config_paths: list[str] = None):
+        super().__init__(parent)
         self._parent = parent
         self._scanning_manager = scanning_manager
 
         self._display = PisakDisplay(parent=self._parent)
         self._keyboards = PisakStackedWidget(parent=self._parent)
+        self.add_item(self._display)
+        self.add_item(self._keyboards)
 
         # todo: create a function that would create and setup a keyboard for each config in keyboards_config_path
 
@@ -19,12 +26,12 @@ class KeyboardDisplayComponent:
         lowercase_config = os.path.join(config_dir, "lowercase_keyboard.yml")
         uppercase_config = os.path.join(config_dir, "uppercase_keyboard.yml")
 
-        self._lower_letters = Keyboard(parent=self._keyboards)
+        self._lower_letters = Keyboard(parent=self._keyboards, strategy=BackNLevelsStrategy(n=3))
         self._lower_letters.implement_layout_from_config(lowercase_config)
         self._keyboards.add_item_reference(self._lower_letters, KeyboardType.LOWERCASE)
         self._keyboards.add_item(self._lower_letters)
 
-        self._upper_letters = Keyboard(parent=self._keyboards)
+        self._upper_letters = Keyboard(parent=self._keyboards, strategy=BackNLevelsStrategy(n=3))
         self._upper_letters.implement_layout_from_config(uppercase_config)
         self._keyboards.add_item_reference(self._upper_letters, KeyboardType.UPPERCASE)
         self._keyboards.add_item(self._upper_letters)
@@ -43,6 +50,8 @@ class KeyboardDisplayComponent:
                                                       stacked_widget=self._keyboards)
         self._button_manager.subscribe(self._keyboards_handler)
 
+        self.set_layout()
+
     @property
     def keyboards(self):
         return self._keyboards
@@ -50,3 +59,8 @@ class KeyboardDisplayComponent:
     @property
     def display(self):
         return self._display
+
+    @property
+    def scannable_items(self) -> list[Any]:
+        return self._keyboards.scannable_items
+
